c005377e1d0576eba07a2db914166ff4ddc92c7d
coreydjordan <83131355+coreydjordan@users.noreply.github.com>
2022-05-23 10:04:45 -0500

    first step of changing into flask app

--- iss_location.py
+++ iss_location.py
@@ -1,26 +1,38 @@
 import requests
 from dotenv import dotenv_values
 
-ENV = dotenv_values()
 
-query = {
-    "lat": '45',
-    "lon": '180'
-}
+from flask import Flask, render_template
+app = Flask(__name__)
 
-response = requests.get(ENV["URL"], params=query)
-# print(response.json())
 
-latitude = response.json()['iss_position']['latitude']
-longitude = response.json()['iss_position']['longitude']
+@app.route('/')
+def index():
 
-loc_query = {
-    "key": ENV["ACCESS_TOKEN"],
-    "lon": longitude,
-    "lat": latitude,
-    "format": "json"
-}
+    
+    ENV = dotenv_values()
 
-loc_res = requests.get(ENV["LOCATION_URL"], params=loc_query)
-print(f"Over {loc_res.json()['address']['country']}")
-# print(longitude,latitude)
+    query = {
+        "lat": '45',
+        "lon": '180'
+    }
+
+    response = requests.get(ENV["URL"], params=query)
+    # print(response.json())
+
+    latitude = response.json()['iss_position']['latitude']
+    longitude = response.json()['iss_position']['longitude']
+
+    loc_query = {
+        "key": ENV["ACCESS_TOKEN"],
+        "lon": longitude,
+        "lat": latitude,
+        "format": "json"
+    }
+
+    loc_res = requests.get(ENV["LOCATION_URL"], params=loc_query)
+    print(f"Over {loc_res.json()['address']['country']}")
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
